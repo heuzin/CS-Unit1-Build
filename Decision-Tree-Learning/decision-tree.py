d1192534875708d915febab0380970199d02e9b1
heuzin <matheusqs.arq@gmail.com>
2020-09-23 21:15:09 -0400

    Decision tree

---
+++ Decision-Tree-Learning/decision-tree.py
@@ -0,0 +1,184 @@
+# Each row is an example.
+eletronics = [
+    ['Laptop', 1000, 500, 500, 'Azus'],
+    ['TV', 1500, 1500, 0, 'LG'],
+    ['TV', 500, 0, 500, 'Samsung'],
+    ['Headphone', 300, 120, 180, 'Apple'],
+    ['Playstation', 300, 300, 0, 'Sony'],
+    ['Xbox', 300, 300, 0, 'Microsoft'],
+    ['Xbox', 250, 200, 100, 'Micriosoft'],
+    ['IPhone', 1000, 200, 800, 'Apple'],
+]
+# Used only to print the tree.
+header = ["name", "amount", "paid", "rest", 'label']
+
+def unique_values(rows, col):
+    # Find unique values for a column.
+    return set([row[col] for row in rows])
+
+def class_counts(rows):
+    # Counts the number of each type of example.
+    counts = {} 
+    for row in rows:
+        # in our dataset format, the label is always the last column
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+def is_number(value):
+    # Check if value is numeric
+    return isinstance(value, int) or isinstance(value, float)
+
+class Question:
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+    def compare(self, example):
+        # Compare the feature value in an example to the
+        # feature value in this question.
+        val = example[self.column]
+        if is_number(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        # Print the question in a readable format.
+        condition = "=="
+        if is_number(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (
+            header[self.column], condition, str(self.value))
+
+def partition(rows, question):
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.compare(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+def gini(rows):
+    counts = class_counts(rows)
+    impurity = 1
+    for lbl in counts:
+        prob_of_lbl = counts[lbl] / float(len(rows))
+        impurity -= prob_of_lbl**2
+    return impurity
+
+def info_gain(left, right, current_uncertainty):
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+def find_best_split(rows):
+    best_gain = 0  # keep track of the best information gain
+    best_question = None  # keep track of the value
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1  # number of columns
+
+    for col in range(n_features):  # for each feature
+
+        values = set([row[col] for row in rows])  # unique values in the column
+
+        for val in values:  # for each value
+
+            question = Question(col, val)
+            # try splitting the dataset
+            true_rows, false_rows = partition(rows, question)
+            # Skip this split if it doesn't divide the dataset.
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+            # Calculate the information gain from this split
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+            if gain >= best_gain:
+                best_gain, best_question = gain, question
+
+    return best_gain, best_question
+
+class Leaf:
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+class Decision_Node:
+    def __init__(self,
+                 question,
+                 true_branch,
+                 false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+def build_tree(rows):
+    gain, question = find_best_split(rows)
+
+    # Base case: no further info gain
+    if gain == 0:
+        return Leaf(rows)
+
+    true_rows, false_rows = partition(rows, question)
+
+    true_branch = build_tree(true_rows)
+
+    false_branch = build_tree(false_rows)
+
+    return Decision_Node(question, true_branch, false_branch)
+
+def print_tree(node, spacing=""):
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print (spacing + "Predict", node.predictions)
+        return
+
+    # Print the question at this node
+    print (spacing + str(node.question))
+
+    # Call this function recursively on the true branch
+    print (spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print (spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
+def classify(row, node):
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    # Decide whether to follow the true-branch or the false-branch.
+    # Compare the feature / value stored in the node, to the example we're considering.
+    if node.question.compare(row):
+        return classify(row, node.true_branch)
+    else:
+        return classify(row, node.false_branch)
+
+def print_leaf(counts):
+    """A nicer way to print the predictions at a leaf."""
+    total = sum(counts.values()) * 1.0
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+if __name__ == '__main__':
+    my_tree = build_tree(eletronics)
+
+    print_tree(my_tree)
+    # Evaluate
+    eletronics = [
+        ['Laptop', 1000, 500, 500],
+        ['TV', 1500, 1500, 0],
+        ['TV', 500, 0, 500],
+        ['Headphone', 300, 120, 180],
+        ['Playstation', 300, 300, 0],
+        ['Xbox', 300, 300, 0],
+        ['Xbox', 250, 200, 100],
+        ['IPhone', 1000, 200, 800],
+    ]
+
+    for row in eletronics:
+        print ("Actual: %s. Predicted: %s" %
+               (row[-1], print_leaf(classify(row, my_tree))))
